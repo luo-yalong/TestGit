4102d382aa12ea0323a24a4e038f1a6ff782d55f
luo-yalong <2192714220@qq.com>
2020-09-05 20:01:10 +0800

    在克隆的地方提交

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,10 +4,15 @@ public class Hello {
         System.out.println("在次测试");
         int i = 0;
 
+
     }
 
     public void demo(){
         System.out.println("你好，世界");
+    }
+
+    public void demo2(){
+        System.out.println("克隆");
     }
 }
 
