470f73ddb6ff297fc2042816296bb0829a5f395b
luo-yalong <2192714220@qq.com>
2020-09-05 19:19:45 +0800

    生成了getSet方法

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -2,9 +2,20 @@ public class TestMain {
 
     private Integer bookName;
     private String bookPrice;
-    private String bookPrice;
-    private String bookPrice;
-    private String bookPrice;
 
+    public Integer getBookName() {
+        return bookName;
+    }
 
+    public void setBookName(Integer bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(String bookPrice) {
+        this.bookPrice = bookPrice;
+    }
 }
