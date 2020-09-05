26aa70e4792ca862c4bebb3c3a087cd0ecb85a66
luo-yalong <70730686+luo-yalong@users.noreply.github.com>
2020-09-05 19:17:42 +0800

    在远程仓库里面修改了一些代码

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -2,5 +2,9 @@ public class TestMain {
 
     private Integer bookName;
     private String bookPrice;
+    private String bookPrice;
+    private String bookPrice;
+    private String bookPrice;
+
 
 }
